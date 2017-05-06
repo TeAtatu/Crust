72ab414ec271726aad7ff69c3c23094316fd0f72
TeAtatu <ropu@outlook.co.nz>
2017-05-06 15:48:15 +1200

    update

--- Main/Shopping/shopping.js
+++ Main/Shopping/shopping.js
@@ -1,8 +1,3 @@
-function remove_liked_disliked(offering_id){
-    document.getElementById(offering_id).classList.remove("offer_like");
-    document.getElementById(offering_id).classList.remove("offer_dislike");
-};
-
 function mark_disliked(offering_id){
     document.getElementById(offering_id).classList.add("offer_dislike");
     document.getElementById(offering_id).classList.remove("offer_like");
@@ -11,5 +6,13 @@ function mark_disliked(offering_id){
 function mark_liked(offering_id){
     document.getElementById(offering_id).classList.add("offer_like");
     document.getElementById(offering_id).classList.remove("offer_dislike");
-    }   
+}  
 
+function view_main(){
+    document.getElementsByClassName("offer_dislike").display = "none";
+    document.getElementsByClassName("offer_like").display = "inline";
+} 
+function view_dislike(){
+    document.getElementsByClassName("offer_dislike").display = "inline";
+    document.getElementsByClassName("offer_like").display = "none";
+} 
