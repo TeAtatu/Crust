ff87f097e6647adb460c6ca634aeacbb6013c599
TeAtatu <ropu@outlook.co.nz>
2017-05-09 23:00:28 +1200

    draft general page

---
+++ HammerHardware/default.js
@@ -0,0 +1,11 @@
+function LoadDetails(){
+    //Add logo
+    logo.src = "images/icon.png";
+
+    //Add organisation name
+    document.getElementById("organisationName").innerHTML = "Hammer hardware Te Atatu";
+
+    //Add organisation name
+    document.getElementById("contactDetails").innerHTML = "09 1234567  admin@abc.com";
+
+}
